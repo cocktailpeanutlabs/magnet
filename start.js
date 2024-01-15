1b6f69fd240fbcc1978f67d74e074a2cbd52393d
cocktailpeanut <cocktailpeanuts@proton.me>
2024-01-15 17:19:58 -0500

    update

--- start.js
+++ start.js
@@ -1,13 +1,9 @@
-module.exports = {
-  daemon: true,
-  run: [{
+module.exports = async (kernel) => {
+  const run = [{
     "method": "shell.run",
     "params": {
       "path": "app",
       "venv": "env",
-      "env": {
-        "IGNORE_MEMORY_EFFICIENT": "1"
-      },
       "message": "python -m demos.magnet_app",
       "on": [{ "event": "/http:\/\/[0-9.:]+/", "done": true }]
     }
@@ -17,4 +13,13 @@ module.exports = {
       "url": "{{input.event[0]}}"
     }
   }]
+  if (kernel.gpu !== 'nvidia') {
+    run[0].params.env = {
+      "IGNORE_MEMORY_EFFICIENT": "1"
+    }
+  }
+  return {
+    daemon: true,
+    run
+  }
 }
